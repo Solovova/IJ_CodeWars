28d422246a4993eae1e51e00c869980d11c219f7
solovova <vsolo0@gmail.com>
2020-05-14 23:17:00 +0300

    v0.22

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,7 +1,7 @@
 import complete.Task23
 
 fun main() {
-    //println(complete.SomeTests().testMutableListDefaultValue())
+    println(complete.SomeTests().testMutableListDefaultValue())
     //println(complete.ExampleRegex().ex1())
     println(
         Task23().sumOfDivided(intArrayOf(107, 158, 204, 100, 118, 123, 126, 110, 116, 100)))
