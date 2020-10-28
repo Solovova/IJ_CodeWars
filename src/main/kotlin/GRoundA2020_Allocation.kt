17906a7e3e57cbbdae99b3962986bbdd2d9d5767
solovova <vsolo0@gmail.com>
2020-10-28 13:59:56 +0200

    0.25

--- src/main/kotlin/GRoundA2020_Allocation.kt
+++ src/main/kotlin/GRoundA2020_Allocation.kt
@@ -1,2 +1,27 @@
 class GRoundA2020_Allocation {
+    private fun solve(n: Int, money: Int, costs: List<Int>): Int {
+        val sortCosts = costs.sorted()
+        var moneySpend = 0
+        var ind = 0
+        while (moneySpend<=money && ind < sortCosts.size) {
+            moneySpend += sortCosts[ind]
+            ind++
+        }
+        return if (moneySpend>money) ind-1 else ind
+    }
+
+    fun main() {
+        val n: Int = readLine()?.toInt() ?: return
+        for (nn in 1..n) {
+            val m1:List<Int> = readLine()?.split(" ")?.map { it.toInt() } ?: return
+            val m2:List<Int> = readLine()?.split(" ")?.map { it.toInt() } ?: return
+            println("Case #$nn: ${solve(m1[0],m1[1],m2)}")
+        }
+    }
+
+    fun run():String {
+        return solve(4,50, listOf(30,30,10,10)).toString()
+    }
+
+
 }
--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,8 +1,10 @@
 import complete.Task23
 
 fun main() {
-    println(complete.SomeTests().testMutableListDefaultValue())
+    //println(complete.SomeTests().testMutableListDefaultValue())
     //println(complete.ExampleRegex().ex1())
-    println(
-        Task23().sumOfDivided(intArrayOf(107, 158, 204, 100, 118, 123, 126, 110, 116, 100)))
+    //println(
+    //    Task23().sumOfDivided(intArrayOf(107, 158, 204, 100, 118, 123, 126, 110, 116, 100)))
+    println(GRoundA2020_Allocation().run())
+    //GRoundA2020_Allocation().runInput()
 }
